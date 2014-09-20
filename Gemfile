a8128fae59b83268a0ca3f2bae273b9c6b4974dd
Jeri Wieringa <jeri.elizabeth@gmail.com>
2014-09-20 10:54:49 -0400

    setup for markdown base modules files

--- Gemfile
+++ Gemfile
@@ -2,14 +2,11 @@
 # the following line to use "https"
 source 'http://rubygems.org'
 
-gem "middleman", "~>3.3.5"
+gem "middleman", "~> 3.3.6"
+gem "middleman-blog", "~> 3.5.3"
 gem 'middleman-deploy', '~> 0.3.0'
 
-# Live-reloading plugin
 gem "middleman-livereload", "~> 3.1.0"
 
-# For faster file watcher updates on Windows:
-gem "wdm", "~> 0.1.0", :platforms => [:mswin, :mingw]
-
-# Windows does not come with time zone data
-gem "tzinfo-data", platforms: [:mswin, :mingw]
+# For feed.xml.builder
+gem "builder", "~> 3.0"
--- config.rb
+++ config.rb
@@ -1,4 +1,36 @@
 ###
+# Blog settings
+###
+
+# Time.zone = "UTC"
+
+activate :blog do |blog|
+  # This will add a prefix to all links, template references and source paths
+  blog.prefix = "modules"
+  blog.permalink = "/{title}.html"
+  # Matcher for blog source files
+  # blog.sources = "{year}-{month}-{day}-{title}.html"
+  # blog.taglink = "tags/{tag}.html"
+  blog.layout = "modules"
+  # blog.summary_separator = /(READMORE)/
+  # blog.summary_length = 250
+  # blog.year_link = "{year}.html"
+  # blog.month_link = "{year}/{month}.html"
+  # blog.day_link = "{year}/{month}/{day}.html"
+  # blog.default_extension = ".markdown"
+
+  blog.tag_template = "tag.html"
+  blog.calendar_template = "calendar.html"
+
+  # Enable pagination
+  # blog.paginate = true
+  # blog.per_page = 10
+  # blog.page_link = "page/{num}"
+end
+
+page "/feed.xml", layout: false
+
+###
 # Compass
 ###
 
@@ -14,10 +46,10 @@
 # Per-page layout changes:
 #
 # With no layout
-# page "/path/to/file.html", :layout => false
+# page "/path/to/file.html", layout: false
 #
 # With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
+# page "/path/to/file.html", layout: :otherlayout
 #
 # A path which all have the same layout
 # with_layout :admin do
@@ -25,8 +57,8 @@
 # end
 
 # Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
+# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
+#  which_fake_page: "Rendering a fake page with a local variable" }
 
 ###
 # Helpers
@@ -36,9 +68,7 @@
 # activate :automatic_image_sizes
 
 # Reload the browser automatically whenever files change
-# configure :development do
-#   activate :livereload
-# end
+# activate :livereload
 
 # Methods defined in the helpers block are available in templates
 # helpers do
@@ -73,7 +103,6 @@ end
 
 activate :deploy do |deploy|
   deploy.method = :git
-  # deploy.build_before = true
   # Optional Settings
   # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
   # deploy.branch   = 'custom-branch' # default: gh-pages
