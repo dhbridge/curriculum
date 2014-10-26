d38aa4b1b6e98823c99ac01678c1a27f30ee4de8
Jeri Wieringa <jeri.elizabeth@gmail.com>
2014-10-26 16:48:42 -0400

    enabling dirty commits

--- config.rb
+++ config.rb
@@ -103,6 +103,7 @@ end
 
 activate :deploy do |deploy|
   deploy.method = :git
+  deploy.clean = false
   # Optional Settings
   # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
   # deploy.branch   = 'custom-branch' # default: gh-pages
